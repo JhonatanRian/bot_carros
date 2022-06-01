4607823c3ee0697c8c5655a3cfce5a9aaeee3386
Jhonatan Rian <jhonatanrian.jr@gmail.com>
2022-06-01 15:47:58 -0300

    bots terminado

--- bot/bot.js
+++ bot/bot.js
@@ -1,78 +1,106 @@
 "use strict";
+const { default: axios } = require('axios');
 const fs = require('fs');
 
 const SOURCE_NAME = "webmotors";
 const objScraper = {
     name: SOURCE_NAME,
-    URL: "https://www.webmotors.com.br/carros/estoque?idcmpint=t1:c17:m07:webmotors:busca::verofertas",
+    URL: "https://www.webmotors.com.br/carros/estoque?tipoveiculo=carros&anunciante=Pessoa%20F%C3%ADsica",
     async scraper(browser) {
-        console.log(`scrapper ${this.URL}`);
-        const self = this;
         const page = await browser.newPage();
         await page.setViewport({ width: 1010, height: 730 });
-        await page.goto(self.URL, {timeout: 5000000});
-        await page.waitForSelector('.sc-gqPbQI')
+        while (true){
+            try{
+                console.log(`scrapper ${this.URL}`);
+                await page.goto(this.URL, {timeout: 5000000});
+                await page.waitForSelector('.sc-gqPbQI')
+                break
+            }catch{
+                console.log("falha na requisição. Tentanto novamente")
+            }
+        }
+        
         let carDivs = await page.$$(".sc-gqPbQI")
         const lista = []
         console.log("Analizando a pagina.")
-        carDivs.forEach((element, index) => {
-            lista.push(this.capture(page, index))
+        await carDivs.forEach(async(element, index) => {
+            let res = await page.evaluate((index)=>{
+                let elements = document.querySelectorAll(".sc-gqPbQI")
+                let element = elements[index]
+                let img
+                try{
+                    img = element.querySelector("img").attributes.src.value
+                }catch{
+                    img = "/static/assets/images/497833.png"
+                }
+                let link = element.querySelector("div div:nth-child(2) a").attributes.href.value
+                let name = element.querySelector("div div:nth-child(2) a div").textContent
+                let price = element.querySelector("div div:nth-child(2) a:nth-child(2) div strong").textContent
+                let km = element.querySelector("div div:nth-child(2) a:nth-child(2) div:nth-child(2) div:nth-child(2)").textContent
+                let ano = element.querySelector("div div:nth-child(2) a:nth-child(2) div:nth-child(2) div:nth-child(1)").textContent
+                return Array.from([img,link,name,price, km, ano])
+            }, index)
+            lista.push(res)
         });
-        await page.waitForTimeout(5000)
-        console.log("Processando analisa e salvando os dados")
-        await this.saving(lista)
-        page.close()
-        page.close()
+        console.log("analise finalizada")
+
+        await page.close()
+
+        return await this.generateObjs(lista)
     },
 
-    async capture(page, i){
-        let index = i
-        let res = await page.evaluate((index)=>{
-            let elements = document.querySelectorAll(".sc-gqPbQI")
-            let element = elements[index]
-            let img
+    async generateObjs(arr){
+        let lto = new Array
+        await arr.forEach(async result => {
+
+            lto.push({
+                img: result[0],
+                source: result[1],
+                name: result[2],
+                price: result[3],
+                km: result[4],
+                year: result[5]
+            })
+            
+        });
+        return lto
+    },
+
+    async get_info(browser, url){
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1010, height: 730 });
+        while (true){
             try{
-                img = element.querySelector("img").attributes.src.value
+                console.log(`scrapper ${url}`);
+                await page.goto(url, {timeout: 5000000});
+                await page.waitForSelector('#VehicleCharacteristic > div > ul')
+                break
             }catch{
-                img = "/static/assets/images/497833.png"
+                console.log("falha na requisição. Tentanto novamente")
             }
-            let link = element.querySelector("div div:nth-child(2) a").attributes.href.value
-            let name = element.querySelector("div div:nth-child(2) a div").textContent
-            let price = element.querySelector("div div:nth-child(2) a:nth-child(2) div strong").textContent
-            let km = element.querySelector("div div:nth-child(2) a:nth-child(2) div:nth-child(2) div:nth-child(2)").textContent
-            let ano = element.querySelector("div div:nth-child(2) a:nth-child(2) div:nth-child(2) div:nth-child(1)").textContent
-            return Array.from([`${img}`,`${link}`,`${name}`,`${price}`, `${km}`, `${ano}`])
-        }, index)
+        }
+        
+        let res = await page.evaluate(async () => {
+            let text = document.querySelector("#VehicleCharacteristic > div > ul").textContent
+            if(text.includes("Único donoSim")){
+                return "1"
+            }else{
+                return "2"
+            }
+        })
+        await page.waitForTimeout(5000)
+        await page.close()
         return res
     },
 
-    async saving(arr){
-        let lto = new Array
-        await arr.forEach(element => {
-            element.then((result) => {
-                lto.push({
-                    img: result[0],
-                    source: result[1],
-                    name: result[2],
-                    price: result[3],
-                    km: result[4],
-                    year: result[5]
-                })
-            }).catch((err) => {
+    async save(data){
+        axios.post("http://127.0.0.1:8000/", JSON.stringify(data), {headers: {'Content-Type': 'application/json'}})
+            .then((resolve) => {
+                console.log(resolve)
+            })
+            .catch((err) => {
                 console.log(err)
-            });
-        });
-        await this.saveJson(lto)
-    },
-
-    async saveJson(data){
-        fs.writeFile(__dirname + "car.json", JSON.stringify(data), function(err) {
-            if(err) {
-                console.log(err);
-            } else {
-                console.log("The file was saved!");
-            }
-        });
+            })
     }
 };
 
--- bot/index.js
+++ bot/index.js
@@ -9,9 +9,22 @@ const puppeteer = require('puppeteer');
 async function main() {
     let browser = await browserInstance()
 
-    await scrapers.scraper(browser)
+    let objs = await scrapers.scraper(browser)
     
-    browser.close()
+    await browser.close()
+
+
+    for (let i = 0 ; i < objs.length ; i++){
+        browser = await browserInstance()
+        let own = await scrapers.get_info(browser, objs[i].source)
+        objs[i].owner = own
+        browser.close()
+
+    }
+
+    await scrapers.save(objs)
+
+
 }
 
 main();
